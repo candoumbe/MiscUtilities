0104e446f7bd8515a9f73f9b276e8adb61d36740
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2018-10-15 17:07:04 +0200

    Initial commit

---
+++ src/Utilities/ReplaceVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Utilities
+{
+    internal class ReplaceVisitor : ExpressionVisitor
+    {
+        private readonly Expression _from, _to;
+
+        public ReplaceVisitor(Expression from, Expression to)
+        {
+            _from = from;
+            _to = to;
+        }        public override Expression Visit(Expression node) => node == _from ? _to : base.Visit(node);
+    }
+}
---
+++ test/Utilities.UnitTests/GuidExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+using Xunit.Categories;
+
+namespace Utilities.UnitTests
+{
+    [UnitTest]
+    [Feature("Guid")]
+    public class GuidExtensionsTests
+    {
+        [Fact]
+        public void Encode()
+        {
+            Guid guid = Guid.NewGuid();
+            string encodedString = guid.Encode();
+
+            encodedString.Should().HaveLength(22);
+            encodedString.Decode().Should().Be(guid);
+        }
+    }
+}
