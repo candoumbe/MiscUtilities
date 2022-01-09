3716d42ca45b18261cbb40f0dfae856641446fd7
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2022-01-09 03:34:24 +0100

    * Added `ToArray<T>` extension methods for `Array` types * Added utility method to get values of a enum type in "regular" arrays (pre `net5.0`)

---
+++ src/Candoumbe.MiscUtilities/ArrayExtensions.cs
@@ -0,0 +1,36 @@
+﻿// "Copyright (c) Cyrille NDOUMBE.
+// Licenced under GNU General Public Licence, version 3.0"
+
+namespace System;
+
+using Linq;
+using Collections.Generic;
+
+/// <summary>
+/// Sets of extension methods for <see cref="Array"/> type.
+/// </summary>
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Converts an <see cref="Array"/>
+    /// </summary>
+    /// <typeparam name="T">Type of items in the array</typeparam>
+    /// <param name="array">The array to convert</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static T[] ToArray<T>(this Array array)
+    {
+        if (array is null)
+        {
+            throw new ArgumentNullException(nameof(array));
+        }
+
+        IList<T> values = new List<T>(array.Length);
+        foreach (T value in array)
+        {
+            values.Add(value);
+        }
+
+        return values.ToArray();
+    }
+}
