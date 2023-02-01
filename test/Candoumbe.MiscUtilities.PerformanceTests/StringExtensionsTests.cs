58ace85e6ce29e3f823ce34bfeb1e578291784a7
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-02-01 19:26:49 +0100

    breaking : drop support of obsolete frameworks and types

--- test/Candoumbe.MiscUtilities.PerformanceTests/StringExtensionsTests.cs
+++ test/Candoumbe.MiscUtilities.PerformanceTests/StringExtensionsTests.cs
@@ -9,7 +9,6 @@ namespace Candoumbe.MiscUtilities.PerformanceTests;
 [MemoryDiagnoser]
 [RPlotExporter]
 [SimpleJob(RuntimeMoniker.NetCoreApp31)]
-[SimpleJob(RuntimeMoniker.Net50)]
 [SimpleJob(RuntimeMoniker.Net60)]
 [SimpleJob(RuntimeMoniker.Net70)]
 public class StringExtensionsTests
