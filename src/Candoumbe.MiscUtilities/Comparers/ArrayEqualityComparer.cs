371f4eb38c5e487408023e3cb855ced5d1d7cc04
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-05-21 14:04:28 +0200

    feat(comparers)!: refactor ArrayEqualityComparer and ArrayEqualityComparerTests
    - move `ArrayEqualityComparer` to `Comparers` namespace
    - update `ArrayEqualityComparerTests` to use `TheoryData` instead of `IEnumerable<object[]>`
    - remove unnecessary using directives in `ArrayEqualityComparerTests`

---
+++ src/Candoumbe.MiscUtilities/Comparers/ArrayEqualityComparer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace Candoumbe.MiscUtilities.Comparers;
+
+/// <summary>
+/// Generic implementation of <see cref="IEqualityComparer{T}"/> suitable for comparing two arrays of <typeparamref name="T"/>
+///
+/// <para>
+/// This implementation is based on <see href="https://exceptionshub.com/gethashcode-on-byte-array.html"/> and relies internally
+/// on <see cref="EqualityComparer{T}.Default"/> to compare items of each array
+/// </para>
+/// </summary>
+/// <typeparam name="T">Type of the collection</typeparam>
+public class ArrayEqualityComparer<T> : IEqualityComparer<T[]>
+{
+    // You could make this a per-instance field with a constructor parameter
+    private static readonly EqualityComparer<T> _elementComparer = EqualityComparer<T>.Default;
+
+    ///<inheritdoc/>
+    public bool Equals(T[] x, T[] y)
+    {
+        bool equals = false;
+        if (x != null && y != null)
+        {
+            if (x == y)
+            {
+                equals = true;
+            }
+            else if (x.Length == y.Length)
+            {
+                switch (x.Length)
+                {
+                    case 0 when y.Length == 0:
+                        equals = true;
+                        break;
+                    default:
+                        int i = 0;
+
+                        do
+                        {
+                            equals = _elementComparer.Equals(x[i], y[i]);
+                            i++;
+                        } while (i < x.Length && equals);
+
+                        break;
+                }
+            }
+        }
+
+        return equals;
+    }
+
+    ///<inheritdoc/>
+    public int GetHashCode(T[] obj)
+    {
+        int hash = 0;
+        if (obj != null)
+        {
+            hash = 17;
+            foreach (T item in obj)
+            {
+                hash = (hash * 31) + _elementComparer.GetHashCode(item);
+            }
+        }
+
+        return hash;
+    }
+}
---
+++ test/Candoumbe.MiscUtilities.UnitTests/Comparers/ArrayEqualityComparerTests.cs
@@ -0,0 +1,58 @@
+﻿// "Copyright (c) Cyrille NDOUMBE.
+// Licenced under GNU General Public Licence, version 3.0"
+
+using System;
+using System.Collections.Generic;
+using Candoumbe.MiscUtilities.Comparers;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Categories;
+
+namespace Candoumbe.MiscUtilities.UnitTests.Comparers
+{
+    [UnitTest]
+    public class ArrayEqualityComparerTests(ITestOutputHelper outputHelper)
+    {
+        private readonly ITestOutputHelper _outputHelper = outputHelper;
+
+        public static TheoryData<IReadOnlyList<byte>, IReadOnlyList<byte>, bool, string> ArrayOfByteEqualCases
+            => new()
+            {
+                { [], null, false, "comparing empty array of bytes to null" },
+                { [], [], true, "comparing Array.Empty<byte>() to Array.Empty<byte>()" },
+                { [1, 2, 3], [1, 2, 3], true, "both arrays are of the same size and contains same data in the same order" },
+                { [1, 2, 3], [1, 3, 3], false, "both arrays are of the same size but the data at not the same at all positions" },
+                { [1, 2, 3], [1, 2, 3, 4], false, "arrays are not of the same size" },
+                { null, null, false, "comparing null to null is inconclusive and should be false by default" }
+            };
+
+        [Theory]
+        [MemberData(nameof(ArrayOfByteEqualCases))]
+        public void TestEquals(byte[] first, byte[] other, bool expected, string reason)
+            => TestEqual(first, other, expected, reason);
+
+        private void TestEqual<T>(T[] first, T[] second, bool expected, string reason)
+        {
+            _outputHelper.WriteLine($"First : '{first.Jsonify()}'");
+            _outputHelper.WriteLine($"Second : '{second.Jsonify()}'");
+
+            ArrayEqualityComparer<T> comparer = new();
+
+            // Act
+            bool actual = comparer.Equals(first, second);
+            int firstHashCode = comparer.GetHashCode(first);
+            int secondHashCode = comparer.GetHashCode(second);
+
+            // Assert
+            actual.Should()
+                  .Be(expected, reason);
+
+            if (expected)
+            {
+                firstHashCode.Should()
+                             .Be(secondHashCode, reason);
+            }
+        }
+    }
+}
