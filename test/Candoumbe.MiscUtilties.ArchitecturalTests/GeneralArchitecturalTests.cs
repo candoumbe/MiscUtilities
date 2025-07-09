254c704daf633bd056236c6c83863d23f3ee9d76
Cyrille NDOUMBE <ndoumbecyrille@gmail.com>
2025-07-09 22:35:09 +0200

    chore(tests): remove warnings
    * remove `namespace does not correspond to file location` warning

--- test/Candoumbe.MiscUtilties.ArchitecturalTests/GeneralArchitecturalTests.cs
+++ test/Candoumbe.MiscUtilties.ArchitecturalTests/GeneralArchitecturalTests.cs
@@ -1,22 +1,27 @@
 ﻿using System.Runtime.CompilerServices;
 using ArchUnitNET.Domain;
+using ArchUnitNET.Domain.Extensions;
 using ArchUnitNET.Fluent;
 using ArchUnitNET.Fluent.Conditions;
 using ArchUnitNET.Loader;
 using ArchUnitNET.xUnitV3;
+using Xunit.Abstractions;
+using Xunit.Categories;
 using Assembly = System.Reflection.Assembly;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
-using Type = System.Type;
-
 
 namespace Candoumbe.MiscUtilties.ArchitecturalTests;
 
+[UnitTest]
 public class GeneralArchitecturalTests
 {
+    private readonly ITestOutputHelper _outputHelper;
 
-    private static readonly Assembly MiscUtiltiesAssembly = typeof(ArrayExtensions).Assembly;
+    public GeneralArchitecturalTests(ITestOutputHelper outputHelper) => _outputHelper = outputHelper;
 
-    private static readonly Architecture Architecure = new ArchLoader().LoadAssemblies(MiscUtiltiesAssembly)
+    private static readonly Assembly s_miscUtilitiesAssembly = typeof(ArrayExtensions).Assembly;
+
+    private static readonly Architecture s_architecture = new ArchLoader().LoadAssemblies(s_miscUtilitiesAssembly)
         .Build();
 
     private static IObjectProvider<MethodMember> ExtensionMethods => MethodMembers()
@@ -29,33 +34,40 @@ public class GeneralArchitecturalTests
         .And()
         .HaveAnyAttributes(typeof(ExtensionAttribute))
         .And()
-        .FollowCustomPredicate(memeber => memeber.Parameters.AtLeastOnce(), "is extension method");
+        .FollowCustomPredicate(member => member.Parameters.AtLeastOnce(), "is extension method");
+
+    private static IObjectProvider<Class> StaticClassesWithExtensionMethods => Classes()
+        .That()
+        .AreAbstract()
+        .And()
+        .FollowCustomPredicate(clazz => ExtensionMethods.GetObjects(s_architecture)
+                                   .Select(extensionMethod => extensionMethod.DeclaringType.FullName)
+                                   .Contains(clazz.FullName),
+                               "is an wrapper of at least one extension method");
 
     [Fact]
-    public void ExtensionMethod_should_be_in_the_same_namespace_as_the_type_it_extends()
+    public void Extension_method_should_be_in_the_same_namespace_as_the_type_it_extends()
     {
         // Arrange
-        IArchRule extensionMethodsShouldResideInSameNamespaceAsTypeThatItExtenssRule = MethodMembers()
+        IArchRule extensionMethodsShouldResideInSameNamespaceAsTypeThatItExtendsRule = MethodMembers()
             .That()
             .Are(ExtensionMethods)
             .Should()
             .FollowCustomCondition(ConditionResult (member) =>
                                    {
                                        Namespace currentNamespace = member.Namespace;
-                                       IType firstParamter = member.Parameters.First();
-                                       Namespace expectedNamespace =  firstParamter.Namespace;
+                                       IType firstParameter = member.Parameters.First();
+                                       Namespace expectedNamespace =  firstParameter.Namespace;
                                        bool isInExpectedNamespace = member.Namespace.Equals(expectedNamespace);
-                                       string extendedType = firstParamter.FullName;
+                                       string extendedType = firstParameter.Name;
 
                                        return new ConditionResult(member,
                                                                   isInExpectedNamespace,
                                                                   $@"should be in ""{expectedNamespace}"" namespace as the ""{extendedType}"" type it extends (currently in ""{currentNamespace.FullName}"")");
                                    },
-                                   "resides in the same namespace as the type it extends");
-
+                                   "resides in the same namespace as the type it extends") ;
 
         // Assert
-        extensionMethodsShouldResideInSameNamespaceAsTypeThatItExtenssRule.Check(Architecure);
-
+        extensionMethodsShouldResideInSameNamespaceAsTypeThatItExtendsRule.Check(s_architecture);
     }
 }
